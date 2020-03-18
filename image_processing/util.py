781e2fe268d9157cdea65bd422f7074d3be5d5c9
naysok <ysok.ysok@gmail.com>
2020-03-18 20:40:41 +0900

    [update] current time

--- image_processing/util.py
+++ image_processing/util.py
@@ -6,9 +6,27 @@
 
 
 import math
+import datetime
+
 
 
 class UTIL():
+
+
+    def get_current_time(self):
+
+        now = datetime.datetime.now()
+        # print("Current Time : {}".format(now))
+
+        ### format
+        f_year = "%04d"%now.year
+        f_month = "%02d"%now.month
+        f_day = "%02d"%now.day
+        f_hour = "%02d"%now.hour
+        f_min = "%02d"%now.minute
+        f_sec = "%02d"%now.second
+
+        return f_year, f_month, f_day, f_hour, f_min, f_sec
 
 
     def remap_number(self, src, old_min, old_max, new_min, new_max):
